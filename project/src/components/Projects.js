77318ba98d680ed58cd497c89bcc7604a2d48776
Olga Cortez <olgalataviacortez@gmail.com>
2021-08-24 09:05:04 -0500

    Initial Project Setup

---
+++ project/src/components/Projects.js
@@ -0,0 +1,9 @@
+const Projects = () => {
+    return ( 
+        <div className="project-page">
+            
+        </div>
+     );
+}
+ 
+export default Projects;
---
+++ project/src/components/Skills.js
@@ -0,0 +1,9 @@
+const Skills = () => {
+    return ( 
+        <div className="skills-page">
+            
+        </div>
+     );
+}
+ 
+export default Skills;
