3624b077c3d1343915d62151e495d133e042d5cf
Olga Cortez <olgalataviacortez@gmail.com>
2021-09-07 12:14:40 -0500

    Added Tablet and Mobile Media Queries

--- project/src/components/home/Home.js
+++ project/src/components/home/Home.js
@@ -5,16 +5,16 @@ const Home = () => {
     return ( 
         <div className="home-page" id="home">
             <div className="full_name">
-                <p className="firstName typewriter">Olga</p> 
+                <p className="firstName">Olga</p> 
                 <img src={Logo} alt="girl sitting at a desk" />
-                <p className="last_name typewriter">Cortez</p>
+                <p className="last_name">Cortez</p>
             </div>
             <div className="port">
-                <p className="portfolio typewriter">Portfolio</p>
+                <p className="portfolio">Portfolio</p>
             </div>
-            <div className="scroll">
+            {/* <div className="scroll">
                 <p>Scroll</p>
-            </div>
+            </div> */}
             <div>
             </div>
         </div>
