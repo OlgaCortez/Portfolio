7ed61c4f9a8ad7cff51c9c5fbc4b970225b6084d
Olga Cortez <olgalataviacortez@gmail.com>
2021-09-07 14:44:05 -0500

    Added video feature to about-me section

--- project/src/components/about/About.js
+++ project/src/components/about/About.js
@@ -1,6 +1,6 @@
-import Desktop from "../../images/Screen.png";
 import Girl from "../../images/About.png";
 import Collage from "../../images/Interest_Tag.png";
+import DesktopVid from "../../images/about-vid.mp4"
 import "./About.css";
 
 const About = () => {
@@ -14,7 +14,9 @@ const About = () => {
                 <p className="dev">Developer</p>
             </div>
             <div className="about-pics">
-                <img src={Desktop} alt="Desktop Screen" />
+                <video autoPlay loop muted id="video">
+                    <source src={DesktopVid} type="video/mp4" />
+                </video>
                 <img src={Girl} alt="Girl typing on Laptop" />
                 <img src={Collage} alt="Collage of the dev's interest" /> 
             </div>
